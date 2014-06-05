4f1b49e8aefc1799a976cb2929e205126987d331
Giuliano Galea <giuliano.galea@gmail.com>
2014-06-05 19:24:37 +0100

    Initial Commit

---
+++ CatsPassion/GGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GGAppDelegate.h
+//  CatsPassion
+//
+//  Created by giullo on 05/06/2014.
+//  Copyright (c) 2014 Giuliano Galea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
